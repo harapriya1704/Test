861a2accd4f180a958136c8e6a01496ede79a0d1
harapriya1704 <123739913+harapriya1704@users.noreply.github.com>
2025-07-04 01:32:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from excel_operations import extract_glassbox_data
 from config import EXCEL_PATH, OUTPUT_EXCEL
 from web_operations import process_glassbox_links
-from file_operations import save_results_to_excel
+from file_operations import initialize_output_excel
 
 def main():
+    initialize_output_excel(OUTPUT_EXCEL)
     data = extract_glassbox_data(EXCEL_PATH)
-    results = process_glassbox_links(data)
-    save_results_to_excel(results, OUTPUT_EXCEL)
+    process_glassbox_links(data)
 
 if __name__ == "__main__":
     main()
